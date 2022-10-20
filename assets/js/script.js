b92796e8b6b83cce4db5b28e00b3aab8e7f102b9
Cassidy Alex <cassidyalex@Cassidys-MBP.lan>
2022-10-20 12:06:18 -0400

    slecet DOM element for and update eventListner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 console.dir(window.document);
 
 // selecting DOM elements by ID
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to dynamically create the task item
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+        //use event object to prevent the browser from carry out its ecpected behavior
+        event.preventDefault();
         // create a new element
         var listItemEl = document.createElement("li");
         // style the new task item
@@ -17,4 +19,4 @@ var createTaskHandler = function() {
 };
 
 // event listerner for button click
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
