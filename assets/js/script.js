704d9ed0e5d40ec6acd883f005fa2cb7602e57db
Cassidy Alex <cassidyalex@Cassidys-MBP.lan>
2022-10-20 13:40:38 -0400

    capture task name and type and append to the task list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-console.dir(window.document);
 
 // selecting DOM elements by ID
 var formEl = document.querySelector("#task-form")
@@ -6,15 +5,26 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to dynamically create the task item
 var createTaskHandler = function(event) {
-        //use event object to prevent the browser from carry out its ecpected behavior
+        // use event object to prevent the browser from carry out its ecpected behavior
         event.preventDefault();
+        // select the value from 'task-name'
+        var taskNameInput = document.querySelector("input[name='task-name").value;
+        // select the value from 'task-type'
+        var taskTypeInput = document.querySelector("select[name='task-type']").value;
         // create a new element
         var listItemEl = document.createElement("li");
         // style the new task item
         listItemEl.className = "task-item";
-        // add the text
-        listItemEl.textContent = "This is a new task.";
-        // apped this element to the task list
+        // create div to hold task info and add to list item
+        var taskInfoEl = document.createElement("div");
+        // give it a class name
+        taskInfoEl.className = "task-info";
+        // add HTML content to div
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+        
+        listItemEl.appendChild(taskInfoEl);
+
+        // add to the task list
         tasksToDoEl.appendChild(listItemEl);
 };
 
